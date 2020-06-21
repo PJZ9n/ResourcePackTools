082916a26df30aea271fcb3a6b156dab76bca395
PJZ9n <pjz9n@yahoo.co.jp>
2020-06-22 07:04:33 +0900

    FileResourcePackの実装

---
+++ src/pjz9n/resourcepacktools/ResourcePackVersion.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Copyright (c) 2020 PJZ9n.
+ *
+ * This file is part of ResourcePackTools.
+ *
+ * ResourcePackTools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ResourcePackTools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ResourcePackTools. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace pjz9n\resourcepacktools;
+
+class ResourcePackVersion
+{
+    /** @var int */
+    private $first;
+
+    /** @var int */
+    private $second;
+
+    /** @var int */
+    private $third;
+
+    public function __construct(int $first, int $second, int $third)
+    {
+        $this->first = $first;
+        $this->second = $second;
+        $this->third = $third;
+    }
+
+    public function toFormatArray(): array
+    {
+        return [$this->first, $this->second, $this->third];
+    }
+
+    /**
+     * @return int
+     */
+    public function getFirst(): int
+    {
+        return $this->first;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSecond(): int
+    {
+        return $this->second;
+    }
+
+    /**
+     * @return int
+     */
+    public function getThird(): int
+    {
+        return $this->third;
+    }
+}
