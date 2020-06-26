5d1d2028b318aa41422bc4e308b267edec6c6d2b
PJZ9n <pjz9n@yahoo.co.jp>
2020-06-27 03:22:35 +0900

    リソースパック管理用関数追加

--- resourcepacktools/src/pjz9n/resourcepacktools/ResourcePack.php
+++ resourcepacktools/src/pjz9n/resourcepacktools/ResourcePack.php
@@ -26,11 +26,13 @@ namespace pjz9n\resourcepacktools;
 use LogicException;
 use pocketmine\resourcepacks\ZippedResourcePack;
 use pocketmine\Server;
+use pocketmine\resourcepacks\ResourcePack as PMResourcePack;
 use ReflectionClass;
 use ReflectionException;
 
 abstract class ResourcePack
 {
+    //TODO: optimize this functions...
     /**
      * @param string $resourcePackPath
      */
@@ -55,5 +57,92 @@ abstract class ResourcePack
         } catch (ReflectionException $reflectionException) {
             throw new LogicException("Caught ReflectionException.");
         }
+    }
+
+    /**
+     * @param PMResourcePack $resourcePack
+     */
+    public static function unRegister(PMResourcePack $resourcePack): void
+    {
+        $resourcePackManager = Server::getInstance()->getResourcePackManager();
+        try {
+            $resourcePackManagerReflection = new ReflectionClass(get_class($resourcePackManager));
+            //ResourcePackManager::$resourcePacks
+            $resourcePacksProperty = $resourcePackManagerReflection->getProperty("resourcePacks");
+            $resourcePacksProperty->setAccessible(true);
+            $resourcePacksValue = $resourcePacksProperty->getValue($resourcePackManager);
+            foreach ($resourcePacksValue as $key => $item) {
+                if ($item === $resourcePack) {
+                    unset($resourcePacksValue[$key]);
+                }
+            }
+            $resourcePacksProperty->setValue($resourcePackManager, $resourcePacksValue);
+            //ResourcePackManager::$uuidList
+            $uuidListProperty = $resourcePackManagerReflection->getProperty("uuidList");
+            $uuidListProperty->setAccessible(true);
+            $uuidListValue = $uuidListProperty->getValue($resourcePackManager);
+            foreach ($uuidListValue as $key => $item) {
+                if ($item === $resourcePack) {
+                    unset($uuidListValue[$key]);
+                }
+            }
+            $uuidListProperty->setValue($resourcePackManager, $uuidListValue);
+        } catch (ReflectionException $reflectionException) {
+            throw new LogicException("Caught ReflectionException.");
+        }
+    }
+
+    /**
+     * @return PMResourcePack[] index => PMResourcePack
+     */
+    public static function getPackList(): array
+    {
+        $resourcePackManager = Server::getInstance()->getResourcePackManager();
+        try {
+            $resourcePackManagerReflection = new ReflectionClass(get_class($resourcePackManager));
+            //ResourcePackManager::$resourcePacks
+            $resourcePacksProperty = $resourcePackManagerReflection->getProperty("resourcePacks");
+            $resourcePacksValue = $resourcePacksProperty->getValue($resourcePackManager);
+            return $resourcePacksValue;
+        } catch (ReflectionException $reflectionException) {
+            throw new LogicException("Caught ReflectionException.");
+        }
+    }
+
+    /**
+     * @return PMResourcePack[] uuid => PMResourcePack
+     */
+    public static function getUuidList(): array
+    {
+        $resourcePackManager = Server::getInstance()->getResourcePackManager();
+        try {
+            $resourcePackManagerReflection = new ReflectionClass(get_class($resourcePackManager));
+            //ResourcePackManager::$uuidList
+            $uuidListProperty = $resourcePackManagerReflection->getProperty("uuidList");
+            $uuidListValue = $uuidListProperty->getValue($resourcePackManager);
+            return $uuidListValue;
+        } catch (ReflectionException $reflectionException) {
+            throw new LogicException("Caught ReflectionException.");
+        }
+    }
+
+    /**
+     * @param int $index
+     *
+     * @return PMResourcePack|null
+     */
+    public static function getPackByIndex(int $index): ?PMResourcePack
+    {
+        return self::getPackList()[$index] ?? null;
+    }
+
+    /**
+     * @param string $uuid
+     *
+     * @return PMResourcePack|null
+     */
+    public static function getPackByUuid(string $uuid): ?PMResourcePack
+    {
+        return self::getUuidList()[$uuid] ?? null;
     }
 }
